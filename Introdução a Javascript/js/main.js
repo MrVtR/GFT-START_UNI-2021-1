c23644b292d4abd3bae65ea9b2c98e4fa87273b8
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-27 23:30:54 -0300

    Adicionado primeiro js do projeto

---
+++ Introdução a Javascript/js/main.js
@@ -0,0 +1,9 @@
+var nome = 'Vítor Ribeiro';
+var idade = 19;
+var idade2 = 11;
+var frase = 'Olá Mundo';
+console.log('Nome: ' + nome + ' tem: ' + idade + ' anos');
+console.log(idade + idade2);
+console.log(frase.replace('Olá', 'Hello'));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
