1b7d6f7fc755de254df61a8cff066ca4e0a77ec3
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-27 23:48:07 -0300

    Adicionado comandos de Array e Dicionário no projeto

--- Introdução a Javascript/js/main.js
+++ Introdução a Javascript/js/main.js
@@ -1,3 +1,4 @@
+console.log('Exercicio de Introdução ao JS:');
 var nome = 'Vítor Ribeiro';
 var idade = 19;
 var idade2 = 11;
@@ -7,3 +8,22 @@ console.log(idade + idade2);
 console.log(frase.replace('Olá', 'Hello'));
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
+
+console.log('Exercicio de Arrays e Dicionários:');
+//Arrays e Dicionários
+var lista = ['maçã', 'banana', 'uva'];
+console.log(lista.push('pêra'));
+console.log(lista.pop());
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(' - '));
+
+var fruta = { nome: 'Maçã', cor: 'Vermelha' };
+console.log(fruta);
+
+var listaFruta = [
+  { nome: 'Maçã', cor: 'Vermelha' },
+  { nome: 'Uva', cor: 'Roxa' },
+];
+console.log(listaFruta);
