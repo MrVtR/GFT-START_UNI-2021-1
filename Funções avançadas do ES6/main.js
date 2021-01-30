25d0d20c9cab6ff3a1a6c5416fdc9785d4689834
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-30 19:36:58 -0300

    Adicionado exemplos de Enhanced Object Funtions

--- Funções avançadas do ES6/main.js
+++ Funções avançadas do ES6/main.js
@@ -14,7 +14,7 @@ var createObject = () => ({ teste: 123 });
 console.log(createObject());
  */
 
-//Lazy evaluation
+/* //Lazy evaluation
 function randomNumber() {
   console.log('Generating a random number...');
   return Math.random() * 10;
@@ -25,4 +25,37 @@ function multiply(a, b = randomNumber()) {
   return a * b;
 }
 console.log(multiply(5));
-console.log(multiply(5));
+console.log(multiply(5)); */
+var prop1 = 'Digital Innovation One';
+function method1() {
+  console.log('method called');
+}
+var obj = {
+  prop1: prop1,
+};
+var objEnhanced = {
+  prop1,
+};
+var objMethod = {
+  method1,
+};
+var objMethodFunction = {
+  sum: function sum(a, b) {
+    return a + b;
+  },
+};
+var objMethodFunctionShort = {
+  sum(a, b) {
+    return a + b;
+  },
+};
+
+var propName = 'test';
+var objName = {
+  [propName + 'concat']: 'prop value',
+};
+
+objMethod.method1();
+console.log(objMethodFunction.sum(5, 6));
+console.log(objMethodFunctionShort.sum(5, 7));
+console.log(objName);
