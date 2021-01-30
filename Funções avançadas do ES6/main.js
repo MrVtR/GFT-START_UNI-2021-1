54bc9284b5bb97abb56a786c7b3b1c4e8df302d1
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-30 19:20:04 -0300

    Adicionado conteúdo sobre Default function arguments

--- Funções avançadas do ES6/main.js
+++ Funções avançadas do ES6/main.js
@@ -1,14 +1,28 @@
-// var sum01 = function (a, b) {
-//   return a + b;
-// };
+/* var sum01 = function (a, b) {
+  return a + b;
+};
 
-//Arrow function -> função anonima enxuta
+Arrow function -> função anonima enxuta
 
-// var sum = (a, b) => a + b;
-// console.log(sum(5, 15));
+var sum = (a, b) => a + b;
+console.log(sum(5, 15));
 
-// var sum = (a, b) => a + b;
-// console.log(sum(5, 15));
+var sum = (a, b) => a + b;
+console.log(sum(5, 15));
 
 var createObject = () => ({ teste: 123 });
 console.log(createObject());
+ */
+
+//Lazy evaluation
+function randomNumber() {
+  console.log('Generating a random number...');
+  return Math.random() * 10;
+}
+
+function multiply(a, b = randomNumber()) {
+  // b = b || 1;
+  return a * b;
+}
+console.log(multiply(5));
+console.log(multiply(5));
