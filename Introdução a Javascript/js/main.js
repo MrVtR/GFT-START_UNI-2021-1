093b88017e5232b518eb763c0080e8f924751d81
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-29 21:04:53 -0300

    Adicionado mais funções ao projeto

--- Introdução a Javascript/js/main.js
+++ Introdução a Javascript/js/main.js
@@ -1,49 +1,49 @@
-// console.log('Exercicio de Introdução ao JS:');
-// var nome = 'Vítor Ribeiro';
-// var idade = 19;
-// var idade2 = 11;
-// var frase = 'Olá Mundo';
-// console.log('Nome: ' + nome + ' tem: ' + idade + ' anos');
-// console.log(idade + idade2);
-// console.log(frase.replace('Olá', 'Hello'));
-// console.log(frase.toUpperCase());
-// console.log(frase.toLowerCase());
+/* console.log('Exercicio de Introdução ao JS:');
+var nome = 'Vítor Ribeiro';
+var idade = 19;
+var idade2 = 11;
+var frase = 'Olá Mundo';
+console.log('Nome: ' + nome + ' tem: ' + idade + ' anos');
+console.log(idade + idade2);
+console.log(frase.replace('Olá', 'Hello'));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
 
-// console.log('Exercicio de Arrays e Dicionários:');
-// //Arrays e Dicionários
-// var lista = ['maçã', 'banana', 'uva'];
-// console.log(lista.push('pêra'));
-// console.log(lista.pop());
-// console.log(lista.length);
-// console.log(lista.reverse());
-// console.log(lista.toString());
-// console.log(lista.join(' - '));
+console.log('Exercicio de Arrays e Dicionários:');
+//Arrays e Dicionários
+var lista = ['maçã', 'banana', 'uva'];
+console.log(lista.push('pêra'));
+console.log(lista.pop());
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(' - '));
 
-// var fruta = { nome: 'Maçã', cor: 'Vermelha' };
-// console.log(fruta);
+var fruta = { nome: 'Maçã', cor: 'Vermelha' };
+console.log(fruta);
 
-// var listaFruta = [
-//   { nome: 'Maçã', cor: 'Vermelha' },
-//   { nome: 'Uva', cor: 'Roxa' },
-// ];
-// console.log(listaFruta);
-// idade = prompt('Qual a sua idade?');
-// if (idade > 18) alert('Maior de idade');
-// else alert('Menor de idade');
+var listaFruta = [
+  { nome: 'Maçã', cor: 'Vermelha' },
+  { nome: 'Uva', cor: 'Roxa' },
+];
+console.log(listaFruta);
+idade = prompt('Qual a sua idade?');
+if (idade > 18) alert('Maior de idade');
+else alert('Menor de idade');
 
-// var count = 0;
-// while (count < 5) {
-//   alert('If: ' + count);
-//   count++;
-// }
+var count = 0;
+while (count < 5) {
+  alert('If: ' + count);
+  count++;
+}
 
-// var count2;
-// for (count = 0; count <= 5; count++) alert(count);
+var count2;
+for (count = 0; count <= 5; count++) alert(count);
 
-// var d = new Date();
-// alert(d.getDay());
-// alert(d.getHours());
-// alert(d.getMinutes());
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
 
 function soma(n1, n2) {
   return n1 + n2;
@@ -61,4 +61,24 @@ function validaIdade(idade) {
 var idade = prompt('Qual a sua idade?');
 console.log(validaIdade(idade));
 alert(soma(5, 10));
-alert(setReplace('Vai Japão', 'Japão', 'Brasil'));
+alert(setReplace('Vai Japão', 'Japão', 'Brasil')); */
+
+function clicou() {
+  document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar';
+}
+function redirecionar() {
+  window.open('https://www.google.com.br');
+  window.location.href = 'https://www.google.com.br';
+}
+function trocar(elemento) {
+  elemento.innerHTML = 'Obrigado por passar o mouse';
+}
+function voltar(elemento) {
+  elemento.innerHTML = 'Passe o mouse aqui';
+}
+function load() {
+  alert('Página carregada');
+}
+function funcaoChange(elemento) {
+  console.log(elemento.value);
+}
