a3d4f5223186d8a3c8f773b7b095a492bb11fcaf
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-30 19:12:08 -0300

    Adicionado exemplos de arrow function

---
+++ Funções avançadas do ES6/main.js
@@ -0,0 +1,14 @@
+// var sum01 = function (a, b) {
+//   return a + b;
+// };
+
+//Arrow function -> função anonima enxuta
+
+// var sum = (a, b) => a + b;
+// console.log(sum(5, 15));
+
+// var sum = (a, b) => a + b;
+// console.log(sum(5, 15));
+
+var createObject = () => ({ teste: 123 });
+console.log(createObject());
