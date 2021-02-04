5b4fc378b0c3ec88d7a1ae93e634558a2daa6a04
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-02-04 11:21:09 -0300

    Adicionado exemplos usando declaração Ref e Out

---
+++ Primeiros passos com .NET + C#/Out/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Out
+{
+  class Program
+  {
+    static void Dividir(int x, int y, out int resultado, out int resto)
+    {
+      resultado = x / y;
+      resto = x % y;
+    }
+    static void Main(string[] args)
+    {
+      Dividir(10, 3, out int resultado, out int resto);
+      Console.WriteLine("{0} {1}", resultado, resto);
+    }
+  }
+}
---
+++ Primeiros passos com .NET + C#/Ref/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ref
+{
+  class Program
+  {
+    static void Inverter(ref int x, ref int y)
+    {
+      int temp = x;
+      x = y;
+      y = temp;
+    }
+
+    public static void Main()
+    {
+      int i = 1, j = 2;
+      Inverter(ref i, ref j);
+      System.Console.WriteLine($"{i} {j}");
+    }
+  }
+}
