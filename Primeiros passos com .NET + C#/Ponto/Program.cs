650267944b3e29e324a0bdacc153f3010006b807
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-02-04 11:01:33 -0300

    Adicionado exemplo de Ponto Cartesiano

---
+++ Primeiros passos com .NET + C#/Ponto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ponto
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Ponto p1 = new Ponto(3, 4);
+      Ponto p2 = new Ponto(10, 20);
+      Console.WriteLine($"Ponto p1: ({p1.x},{p1.y})");
+      Console.WriteLine($"Ponto p2: ({p2.x},{p2.y})");
+    }
+  }
+  public class Ponto
+  {
+    public int x, y;
+    public Ponto(int x, int y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+  }
+}
