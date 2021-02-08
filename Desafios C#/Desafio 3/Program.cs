a06335152912a0534981e3efe0cbd5763676cdff
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-02-08 00:56:06 -0300

    Adicionado Desafio 3

---
+++ Desafios C#/Desafio 3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Desafio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String p1 = Console.ReadLine();
+            String p2 = Console.ReadLine();
+            String p3 = Console.ReadLine();
+
+            if ((p1=="vertebrado") && (p2=="ave") && (p3=="carnivoro"))
+            {
+                //complete o desafio
+                Console.WriteLine("aguia\n");
+            }
+
+            if ((p1=="vertebrado") && (p2=="ave") && (p3=="onivoro"))
+            {
+                //complete o desafio
+                Console.WriteLine("pomba\n");
+            }
+
+            if ((p1=="vertebrado") && (p2=="mamifero") && (p3=="onivoro"))
+            {
+                //complete o desafio
+                Console.WriteLine("homem\n");
+            }
+
+            if ((p1=="vertebrado") && (p2=="mamifero") && (p3=="herbivoro"))
+            {
+                //complete o desafio
+                Console.WriteLine("vaca\n");
+            }
+
+            if ((p1=="invertebrado") && (p2=="inseto") && (p3=="hematofago"))
+            {
+                //complete o desafio
+                Console.WriteLine("pulga\n");
+            }
+
+            if ((p1=="invertebrado") && (p2=="inseto") && (p3=="herbivoro"))
+            {
+                //complete o desafio
+                Console.WriteLine("lagarta\n");
+            }
+
+            if ((p1== "invertebrado") && (p2== "anelideo") && (p3== "hematofago"))
+            {
+                Console.WriteLine("sanguessuga\n");
+            }
+            if ((p1== "invertebrado") && (p2== "anelideo") && (p3== "onivoro"))
+            {
+                Console.WriteLine("minhoca\n");
+            }
+        }
+    }
+}
