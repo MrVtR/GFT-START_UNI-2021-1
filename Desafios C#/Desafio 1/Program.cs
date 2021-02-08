f1050252d88331e7b1d56d4ca5e4e0c7d3637966
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-02-08 00:04:00 -0300

    Adicionado desafio 1

---
+++ Desafios C#/Desafio 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Desafio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] viagem = Console.ReadLine().Split(' ');
+            int horas = Int32.Parse(viagem[0]);
+            int vel = Int32.Parse(viagem[1]);
+            float qtdeLitros = ((float)horas * (float)vel)/12;
+            Console.WriteLine(qtdeLitros.ToString("0.000"));
+        }
+    }
+}
