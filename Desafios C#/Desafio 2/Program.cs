04bd28df66e80282b64f0c7007c00d9bed829f3b
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-02-08 00:43:10 -0300

    Adicionado desafio 2

---
+++ Desafios C#/Desafio 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+            int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+            int totalDeFigurinhas = 0;
+            List<int> album = new List<int>();
+            for (int i = 0; i < numeroDeFigurinhasCompradas; i++)
+            {
+                album.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            var diff = album.Distinct().ToList();
+            totalDeFigurinhas = numeroDeFigurinhas - diff.Count;
+            Console.WriteLine(totalDeFigurinhas);
+        }
+    }
+}
